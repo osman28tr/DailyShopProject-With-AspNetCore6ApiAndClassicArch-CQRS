205be926479c48e6a0e3a99b2a1859e6cca35b07
osman28tr <osmantonbul2001@gmail.com>
2023-10-20 22:10:54 +0300

    Category ve productlar ile ilgili servis işlemleri gerçekleştirildi.

---
+++ DailyShop.Business/Services/Repositories/IProductImageRepository.cs
@@ -0,0 +1,9 @@
+﻿using Core.Persistence.Repositories;
+using DailyShop.Entities.Concrete;
+
+namespace DailyShop.Business.Services.Repositories
+{
+    public interface IProductImageRepository:IAsyncRepository<ProductImage>,IRepository<ProductImage>
+    {
+    }
+}
---
+++ DailyShop.Business/Services/Repositories/IProductSizeRepository.cs
@@ -0,0 +1,9 @@
+﻿using Core.Persistence.Repositories;
+using DailyShop.Entities.Concrete;
+
+namespace DailyShop.Business.Services.Repositories
+{
+    public interface IProductSizeRepository:IAsyncRepository<ProductSize>,IRepository<ProductSize>
+    {
+    }
+}
