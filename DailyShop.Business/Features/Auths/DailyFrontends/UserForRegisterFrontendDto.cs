a0a79d1e43a39cadaf6a8c6725b5adebbbce1834
osman28tr <osmantonbul2001@gmail.com>
2023-10-22 19:42:49 +0300

    deneme

--- DailyShop.Business/Features/Auths/DailyFrontends/UserForRegisterFrontendDto.cs
+++ DailyShop.Business/Features/Auths/DailyFrontends/UserForRegisterFrontendDto.cs
@@ -8,12 +8,12 @@ namespace DailyShop.Business.Features.Auths.DailyFrontends
 {
 	public class UserForRegisterFrontendDto
 	{
-		//public string email { get; set; }
-		//public string password { get; set; }
-		////public string confirmpassword { get; set; }
-		//public string name { get; set; }
-		////public string role { get; set; }
-		//public string surname { get; set; }
-		//public string phonenumber { get; set; }
+		public string email { get; set; }
+		public string password { get; set; }
+		//public string confirmpassword { get; set; }
+		public string name { get; set; }
+		//public string role { get; set; }
+		public string surname { get; set; }
+		public string phonenumber { get; set; }
 	}
 }
