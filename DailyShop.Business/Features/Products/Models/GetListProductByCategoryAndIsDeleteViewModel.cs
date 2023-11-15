72b1142de82ac65d26e6bc3bee7493d9645209a1
osman28tr <osmantonbul2001@gmail.com>
2023-11-15 23:37:09 +0300

    Kategoriye göre ürün listeleme ve ürün detaylarının listelenmesi işleminde view modellerdeki duplicated proplar giderildi.

--- DailyShop.Business/Features/Products/Models/GetListProductByCategoryAndIsDeleteViewModel.cs
+++ DailyShop.Business/Features/Products/Models/GetListProductByCategoryAndIsDeleteViewModel.cs
@@ -7,25 +7,9 @@ using System.Threading.Tasks;
 
 namespace DailyShop.Business.Features.Products.Models
 {
-    public class GetListProductByCategoryAndIsDeleteViewModel
+    public class GetListProductByCategoryAndIsDeleteViewModel:ProductViewModel
     {
-        public GetListProductByCategoryAndIsDeleteViewModel()
-        {
-            ReviewsModel = new List<GetListReviewByProductViewModel>();
-        }
-        public int Id { get; set; }
-        public string? Name { get; set; }
-        public decimal? Price { get; set; }
-        public string? BodyImage { get; set; }
-        public string? Description { get; set; }
-        public string? Status { get; set; }
-        public int? Stock { get; set; }
-        public string? UserName { get; set; }
-        public byte? Rating { get; set; }          
-        public ICollection<string>? ProductImages { get; set; }
-        public ICollection<string>? Colors { get; set; }
-        public ICollection<string>? Sizes { get; set; }
-        public ICollection<GetListReviewByProductViewModel>? ReviewsModel { get; set; }
+
         //public ICollection<Review>? Reviews { get; set; }
     }
 }
---
+++ DailyShop.Business/Features/Products/Models/ProductViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyShop.Business.Features.Products.Models
+{
+    public class ProductViewModel
+    {
+        public ProductViewModel()
+        {
+            ReviewsModel = new List<GetListReviewByProductViewModel>();
+        }
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public decimal? Price { get; set; }
+        public string? BodyImage { get; set; }
+        public string? Description { get; set; }
+        public string? Status { get; set; }
+        public int? Stock { get; set; }
+        public string? UserName { get; set; }
+        public byte? Rating { get; set; }
+        public ICollection<string>? ProductImages { get; set; }
+        public ICollection<string>? Colors { get; set; }
+        public ICollection<string>? Sizes { get; set; }
+        public ICollection<GetListReviewByProductViewModel>? ReviewsModel { get; set; }
+    }
+}
