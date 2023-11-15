61f7158d001ac1a265afdb38ccb6b01f9ca96622
osman28tr <osmantonbul2001@gmail.com>
2023-11-15 23:26:53 +0300

    Product color fluent api migration ile ilgili duplicated code giderildi.

--- DailyShop.DataAccess/Migrations/20231113163855_exampl.cs
+++ DailyShop.DataAccess/Migrations/20231113163855_exampl.cs
@@ -45,35 +45,6 @@ namespace DailyShop.DataAccess.Migrations
         {
             migrationBuilder.DropTable(
                 name: "ProductColor");
-
-            migrationBuilder.CreateTable(
-                name: "ColorProduct",
-                columns: table => new
-                {
-                    ColorsId = table.Column<int>(type: "int", nullable: false),
-                    ProductsId = table.Column<int>(type: "int", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_ColorProduct", x => new { x.ColorsId, x.ProductsId });
-                    table.ForeignKey(
-                        name: "FK_ColorProduct_Colors_ColorsId",
-                        column: x => x.ColorsId,
-                        principalTable: "Colors",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                    table.ForeignKey(
-                        name: "FK_ColorProduct_Products_ProductsId",
-                        column: x => x.ProductsId,
-                        principalTable: "Products",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                });
-
-            migrationBuilder.CreateIndex(
-                name: "IX_ColorProduct_ProductsId",
-                table: "ColorProduct",
-                column: "ProductsId");
         }
     }
 }
