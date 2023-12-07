99af4c7a992863c24b8af6ed6b9f619a21828ee5
osman28tr <osmantonbul2001@gmail.com>
2023-12-07 18:26:56 +0300

    Admin tarafında ürünlere ait resimlerin getirilmesi işlemi gerçekleştirildi.

--- DailyShop.API/Areas/Admin/Controllers/UsersController.cs
+++ DailyShop.API/Areas/Admin/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 ﻿using Core.CrossCuttingConcerns.Exceptions;
 using DailyShop.API.Helpers;
 using DailyShop.Business.Features.Products.Queries.GetListProductByUserId;
+using DailyShop.Entities.Concrete;
 using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -12,13 +13,34 @@ namespace DailyShop.API.Areas.Admin.Controllers
     [ApiController]
     public class UsersController : BaseController
     {
+        private readonly ImageHelper _imageHelper;
+
+        public UsersController(ImageHelper imageHelper)
+        {
+            _imageHelper = imageHelper;
+        }
         [HttpGet("{userId:int}/Products")]
         public async Task<IActionResult> GetProductByUserId(int userId)
         {
             var productValues = await Mediator?.Send(new GetListProductByUserIdQuery() { UserId = userId })!;
             if (productValues == null)
                 throw new BusinessException("Bu kullanıcıya ait bir ürün bulunamadı veya kaldırıldı! ");
+            foreach (var product in productValues)
+            {
+                product.BodyImage = GetImageByHelper(product.BodyImage);
+                if (product.ProductImages != null)
+                {
+                    product.ProductImages = product.ProductImages.Select(x =>
+                        GetImageByHelper(x)).ToList();
+                }
+            }
             return Ok(new { data = productValues, message = "Ürün verileri başarıyla getirildi." });
+        }
+        [NonAction]
+        public string GetImageByHelper(string imageName)
+        {
+            string getImage = _imageHelper.GetImage(Request.Scheme, Request.Host, Request.PathBase, imageName);
+            return getImage;
         }
     }
 }
