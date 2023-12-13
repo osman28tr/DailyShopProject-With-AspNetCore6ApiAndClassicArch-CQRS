d7e19c2a95ea9177427df1cb870c9301a8a42462
osman28tr <osmantonbul2001@gmail.com>
2023-12-13 14:17:25 +0300

    Admin tarafında kullanıcıya ait ürünlerin listelenmesi hızlandırıldı.

--- DailyShop.Business/Features/Products/Queries/GetListProductByUserId/GetListProductByUserIdQuery.cs
+++ DailyShop.Business/Features/Products/Queries/GetListProductByUserId/GetListProductByUserIdQuery.cs
@@ -7,6 +7,7 @@ using AutoMapper;
 using Core.CrossCuttingConcerns.Exceptions;
 using DailyShop.Business.Features.Products.Models;
 using DailyShop.Business.Services.Repositories;
+using DailyShop.Business.Services.Repositories.Dapper;
 using DailyShop.Entities.Concrete;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -19,34 +20,50 @@ namespace DailyShop.Business.Features.Products.Queries.GetListProductByUserId
         public class GetProductByUserIdQueryHandler:IRequestHandler<GetListProductByUserIdQuery,List<GetListProductByUserIdViewModel>>
         {
             private readonly IProductRepository _productRepository;
+            private readonly IDpProductRepository _dpProductRepository;
             private readonly IMapper _mapper;
 
-            public GetProductByUserIdQueryHandler(IProductRepository productRepository, IMapper mapper)
+            public GetProductByUserIdQueryHandler(IProductRepository productRepository,IDpProductRepository dpProductRepository, IMapper mapper)
             {
                 _productRepository = productRepository;
+                _dpProductRepository = dpProductRepository;
                 _mapper = mapper;
             }
             public async Task<List<GetListProductByUserIdViewModel>> Handle(GetListProductByUserIdQuery request, CancellationToken cancellationToken)
             {
                 var products = await _productRepository.Query().Where(p => p.UserId == request.UserId)
-                    .Include(u => u.User)
-                    .Include(c => c.ProductImages).Include(r => r.Reviews).ThenInclude(ap => ap.AppUser).Include(c => c.Colors)
-                    .ThenInclude(c => c.Color)
-                    .Include(s => s.Sizes).ThenInclude(s => s.Size).ToListAsync();
-
-                //var products = await _productRepository.Query().Where(p => p.UserId == request.UserId)
-                //    .Include(u => u.User).ToListAsync();
-
-                //var productImages = products.AsQueryable().Include(x => x.ProductImages).LoadAsync();
-                //var reviews = products.AsQueryable().Include(x => x.Reviews).LoadAsync();
-                //var colors = products.AsQueryable().Include(x=>x.Colors).ThenInclude(x=>x.Color).LoadAsync();
-                //var sizes = products.AsQueryable().Include(x => x.Sizes).ThenInclude(x => x.Size).LoadAsync();
-
+                    .Include(r => r.Reviews).ThenInclude(ap => ap.AppUser).ToListAsync();
 
                 if (products == null) throw new BusinessException("Bu kullanıcıya ait bir ürün bulunamadı.");
 
                 var mappedProducts = _mapper.Map<List<GetListProductByUserIdViewModel>>(products);
-
+                foreach (var mappedProduct in mappedProducts)
+                {
+                    var productColors = await _dpProductRepository.GetProductDetailColorByIdAsync(mappedProduct.Id);
+                    if (productColors != null)
+                    {
+                        foreach (var productColor in productColors)
+                        {
+                            mappedProduct.Colors.Add(productColor);
+                        }
+                    }
+                    var productSizes = await _dpProductRepository.GetProductDetailSizeByIdAsync(mappedProduct.Id);
+                    if (productSizes != null)
+                    {
+                        foreach (var productSize in productSizes)
+                        {
+                            mappedProduct.Sizes.Add(productSize);
+                        }
+                    }
+                    var productImages = await _dpProductRepository.GetProductDetailImageByIdAsync(mappedProduct.Id);
+                    if (productImages != null)
+                    {
+                        foreach (var productImage in productImages)
+                        {
+                            mappedProduct.ProductImages.Add(productImage);
+                        }
+                    }
+                }
                 foreach (var mappedProduct in mappedProducts)
                 {
                     foreach (var product in products)
