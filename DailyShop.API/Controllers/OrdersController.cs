abfbd083a3d0783e8230a080244b93113c13795d
osman28tr <osmantonbul2001@gmail.com>
2023-12-13 17:50:07 +0300

    Order için repository pattern ve cqrs pattern implemente edildi.

---
+++ DailyShop.API/Controllers/OrdersController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DailyShop.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+    }
+}
---
+++ DailyShop.DataAccess/Concrete/EntityFramework/Repositories/OrderRepository.cs
@@ -0,0 +1,20 @@
+﻿using Core.Persistence.Repositories;
+using Core.Security.Entities;
+using DailyShop.Business.Services.Repositories;
+using DailyShop.DataAccess.Concrete.EntityFramework.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DailyShop.Entities.Concrete;
+
+namespace DailyShop.DataAccess.Concrete.EntityFramework.Repositories
+{
+    public class OrderRepository: EfRepositoryBase<Order, DailyShopContext>, IOrderRepository
+    {
+        public OrderRepository(DailyShopContext context) : base(context)
+        {
+        }
+    }
+}
