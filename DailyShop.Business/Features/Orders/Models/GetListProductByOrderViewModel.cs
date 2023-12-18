c7fff712532e46762260b0e85a80e3f4c6058936
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-12-18 16:10:11 +0300

    order itemin product'ının resmi getirildi

--- DailyShop.Business/Features/Orders/Models/GetListProductByOrderViewModel.cs
+++ DailyShop.Business/Features/Orders/Models/GetListProductByOrderViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace DailyShop.Business.Features.Orders.Models
@@ -12,5 +13,7 @@ namespace DailyShop.Business.Features.Orders.Models
         public string? Name { get; set; }
         public byte? Rating { get; set; }
         public decimal? Price { get; set; }
+        [JsonPropertyName("image")]
+        public string? BodyImage { get; set; }
     }
 }
