b913334bff50486b314695f1e58040989d54c329
osman28tr <osmantonbul2001@gmail.com>
2023-12-19 14:57:12 +0300

    Favorilerde ürünün ismi getirildi.

--- DailyShop.Business/Features/Favorites/Models/GetListProductAtFavorite.cs
+++ DailyShop.Business/Features/Favorites/Models/GetListProductAtFavorite.cs
@@ -10,6 +10,7 @@ namespace DailyShop.Business.Features.Favorites.Models
     public class GetListProductAtFavorite
     {
         public int? Id { get; set; }
+        public string? Name { get; set; }
         public decimal? Price { get; set; }
         [JsonPropertyName("image")]
         public string? BodyImage { get; set; }
