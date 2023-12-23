683455e26f5ae446d0a16a082474ed757acedbe6
osman28tr <osmantonbul2001@gmail.com>
2023-12-24 01:48:45 +0300

    Yorumlara alt yorum ekleme işlemi ile ilgili entity konfigürasyonu gerçekleştirildi ve veritabanına migrate edildi.

--- DailyShop.DataAccess/Migrations/20231223224646_add_parentreview_at_reviewtable.Designer.cs
+++ DailyShop.DataAccess/Migrations/20231223224646_add_parentreview_at_reviewtable.Designer.cs
@@ -4,6 +4,7 @@ using DailyShop.DataAccess.Concrete.EntityFramework.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DailyShop.DataAccess.Migrations
 {
     [DbContext(typeof(DailyShopContext))]
-    partial class DailyShopContextModelSnapshot : ModelSnapshot
+    [Migration("20231223224646_add_parentreview_at_reviewtable")]
+    partial class add_parentreview_at_reviewtable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- DailyShop.Entities/Concrete/Review.cs
+++ DailyShop.Entities/Concrete/Review.cs
@@ -1,6 +1,7 @@
 ﻿using Core.Persistence.Repositories;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,7 @@ namespace DailyShop.Entities.Concrete
     {
         public int? ProductId { get; set; }
         public int? AppUserId { get; set; }
+        public int? ParentReviewId { get; set; }
         public string? Name { get; set; }
         public byte? Rating { get; set; }
         public string? Description { get; set; }
@@ -18,6 +20,8 @@ namespace DailyShop.Entities.Concrete
         public string? Status { get; set; }
         public Product? Product { get; set; }
         public AppUser? AppUser { get; set; }
+        [ForeignKey("ParentReviewId")]
+        public ICollection<Review> SubReviews { get; set; }
         public Review()
         {
             
