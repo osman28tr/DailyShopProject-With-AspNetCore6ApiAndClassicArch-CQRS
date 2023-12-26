c2635b04f0690a7264f1562a23a94767aa0d0164
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-12-26 22:29:34 +0300

    yorumlar revize edildi ve GetListReviewByProductViewModel' e kullanıcı bu ürünü satın aldı mı bilgisi eklendi

--- DailyShop.Business/Features/Products/Models/GetListReviewByProductViewModel.cs
+++ DailyShop.Business/Features/Products/Models/GetListReviewByProductViewModel.cs
@@ -7,22 +7,24 @@ using System.Threading.Tasks;
 
 namespace DailyShop.Business.Features.Products.Models
 {
-    public class GetListReviewByProductViewModel
-    {
-        [JsonPropertyName("id")]
-        public int Id { get; set; }
-        [JsonPropertyName("rating")]
-        public byte? ReviewRating { get; set; }
-        [JsonPropertyName("comment")]
-        public string? ReviewDescription { get; set; }
-        [JsonPropertyName("status")]
-        public string? ReviewStatus { get; set; }
-        [JsonPropertyName("date")]
-        public DateTime? ReviewCreatedDate { get; set; }
-        public DateTime? ReviewUpdatedDate { get; set; }
-        [JsonPropertyName("user")]
-        public ReviewUser? User { get; set; }
-    }
+	public class GetListReviewByProductViewModel
+	{
+		[JsonPropertyName("id")]
+		public int Id { get; set; }
+		[JsonPropertyName("rating")]
+		public byte? ReviewRating { get; set; }
+		[JsonPropertyName("comment")]
+		public string? ReviewDescription { get; set; }
+		[JsonPropertyName("status")]
+		public string? ReviewStatus { get; set; }
+		[JsonPropertyName("date")]
+		public DateTime? ReviewCreatedDate { get; set; }
+		public DateTime? ReviewUpdatedDate { get; set; }
+		[JsonPropertyName("user")]
+		public ReviewUser? User { get; set; }
+		[JsonPropertyName("userPurchasedThisProduct")]
+		public bool? UserPurchasedThisProduct { get; set; }
+	}
 }
 
 public class ReviewUser
@@ -33,6 +35,6 @@ public class ReviewUser
 	public string? Name { get; set; }
 	[JsonPropertyName("profileImage")]
 	public string? Image { get; set; }
-    [JsonPropertyName("email")]
-    public string? Email { get; set; }
+	[JsonPropertyName("email")]
+	public string? Email { get; set; }
 }
--- DailyShop.Business/Features/Products/Queries/GetProductDetailById/GetProductDetailByIdQuery.cs
+++ DailyShop.Business/Features/Products/Queries/GetProductDetailById/GetProductDetailByIdQuery.cs
@@ -20,13 +20,15 @@ public class GetProductDetailByIdQuery : IRequest<GetProductDetailByIdViewModel>
         private readonly IProductRepository _productRepository;
         private readonly IDpProductRepository _dpProductRepository;
         private readonly IAppUserRepository _appUserRepository;
+	    private readonly IOrderRepository _orderRepository;
         private readonly IMapper _mapper;
 
-        public GetByIdProductDetailQueryHandler(IProductRepository productRepository,IDpProductRepository dpProductRepository, IAppUserRepository appUserRepository, IMapper mapper)
+        public GetByIdProductDetailQueryHandler(IProductRepository productRepository,IDpProductRepository dpProductRepository, IAppUserRepository appUserRepository, IOrderRepository orderRepository, IMapper mapper)
         {
             _productRepository = productRepository;
             _appUserRepository = appUserRepository;
             _dpProductRepository = dpProductRepository;
+            _orderRepository = orderRepository;
             _mapper = mapper;
         }
 
@@ -41,9 +43,9 @@ public class GetProductDetailByIdQuery : IRequest<GetProductDetailByIdViewModel>
             var colors = await _dpProductRepository.GetProductDetailColorByIdAsync(request.ProductId);
             var sizes = await _dpProductRepository.GetProductDetailSizeByIdAsync(request.ProductId);
             var productImages = await _dpProductRepository.GetProductDetailImageByIdAsync(request.ProductId);
-            string productUserName = await _dpProductRepository.GetProductDetailUserByIdAsync(request.ProductId);
+            var productUserName = await _dpProductRepository.GetProductDetailUserByIdAsync(request.ProductId);
 
-            var user = await _appUserRepository.Query().FirstOrDefaultAsync(x => x.Id == request.UserId);
+            var user = await _appUserRepository.Query().FirstOrDefaultAsync(x => x.Id == request.UserId, cancellationToken: cancellationToken);
 
             if ((product?.IsApproved == null || (bool)!product.IsApproved) || product.IsDeleted == true)
             {
@@ -67,50 +69,46 @@ public class GetProductDetailByIdQuery : IRequest<GetProductDetailByIdViewModel>
             mappedProduct.Rating = product.Reviews is {Count: > 0} ? (byte)product.Reviews.Average(x => x.Rating)! : (byte)0;
 
             if (product.Reviews == null) return mappedProduct;
+
+            var allOrder = await _orderRepository.Query()
+	            .Include(o => o.OrderItems)
+	            .ToListAsync(cancellationToken: cancellationToken);
+
             foreach (var review in product.Reviews)
             {
-                GetListReviewByProductViewModel reviewModel = new()
-                {
-                    ReviewDescription = review.Description,
-                    ReviewRating = review.Rating,
-                    ReviewStatus = review.Status,
-                    ReviewCreatedDate = review.CreatedAt,
-                    ReviewUpdatedDate = review.UpdatedAt,
-                    Id = review.Id,
-                    User = new ReviewUser()
-					{
-						Id = review.AppUser.Id,
-						Name = review.AppUser.FirstName + " " + review.AppUser.LastName,
-						Image = review.AppUser.ProfileImage,
-                        Email = review.AppUser.Email
-					}
-                };
-                mappedProduct.ReviewsModel.Add(reviewModel);
+	            if (review.AppUser == null) continue;
+	            GetListReviewByProductViewModel reviewModel = new()
+	            {
+		            ReviewDescription = review.Description,
+		            ReviewRating = review.Rating,
+		            ReviewStatus = review.Status,
+		            ReviewCreatedDate = review.CreatedAt,
+		            ReviewUpdatedDate = review.UpdatedAt,
+		            Id = review.Id,
+		            User = new ReviewUser()
+		            {
+			            Id = review.AppUser.Id,
+			            Name = review.AppUser.FirstName + " " + review.AppUser.LastName,
+			            Image = review.AppUser.ProfileImage,
+			            Email = review.AppUser.Email
+		            },
+		            UserPurchasedThisProduct = allOrder.Any(x => x.OrderItems != null && x.UserId == review.AppUser.Id && x.OrderItems.Any(orderItem => orderItem.ProductId == product.Id))
+	            };
+	            mappedProduct.ReviewsModel?.Add(reviewModel);
             }
             
             if (colors.Any())
-            {
                 foreach (var color in colors)
-                {
-                    mappedProduct.Colors.Add(color);
-                }
-            }
+                    mappedProduct.Colors?.Add(color);
 
             if (sizes.Any())
-            {
                 foreach (var size in sizes)
-                {
-                    mappedProduct.Sizes.Add(size);
-                }
-            }
+                    mappedProduct.Sizes?.Add(size);
 
             if (productImages.Any())
-            {
                 foreach (var productImage in productImages)
-                {
-                    mappedProduct.ProductImages.Add(productImage);
-                }
-            }
+                    mappedProduct.ProductImages?.Add(productImage);
+
             return mappedProduct;
         }
     }
