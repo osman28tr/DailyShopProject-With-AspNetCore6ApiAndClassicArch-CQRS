f6edd240b93fe3ebbf9870a96ff22d9c6f39abba
osman28tr <osmantonbul2001@gmail.com>
2024-01-01 01:58:22 +0300

    ReportReview ve user dtolar'a jsonproplar eklendi.

--- DailyShop.Business/Features/AppUsers/Dtos/ReportUserDto.cs
+++ DailyShop.Business/Features/AppUsers/Dtos/ReportUserDto.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace DailyShop.Business.Features.AppUsers.Dtos
 {
     public class ReportUserDto
     {
+        [JsonPropertyName("message")]
         public string? ReportedMessage { get; set; }
     }
 }
--- DailyShop.Business/Features/Reviews/Dtos/ReportReviewDto.cs
+++ DailyShop.Business/Features/Reviews/Dtos/ReportReviewDto.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace DailyShop.Business.Features.Reviews.Dtos
 {
     public class ReportReviewDto
     {
+        [JsonPropertyName("message")]
         public string? ReportedMessage { get; set; }
     }
 }
